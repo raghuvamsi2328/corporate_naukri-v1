351ac899ad5db7cd0d18aee47aab6c406eda8a02
atla-naveen_bflghec <atla.naveen@bajajfinserv.in>
2026-01-31 15:54:49 +0530

    angular v 16

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TrackerComponent } from './components/tracker/tracker.component';
+import { FormsComponent } from './components/forms/forms.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginSignupComponent } from './pages/login-signup/login-signup.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { UserDetailsComponent } from './pages/user-details/user-details.component';
+import { PaymentComponent } from './pages/payment/payment.component';
+import { StatusComponent } from './pages/status/status.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    TrackerComponent,
+    FormsComponent,
+    DashboardComponent,
+    LoginSignupComponent,
+    LandingComponent,
+    UserDetailsComponent,
+    PaymentComponent,
+    StatusComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
